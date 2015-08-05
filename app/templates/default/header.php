ab640d052aab043d8738a744699988a7bd10e6c8
Alex Shalaiev <itdep24@gmail.com>
2015-08-06 00:10:44 +0300

    clean files

--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -38,6 +38,19 @@ $hooks = Hooks::get();
 
 </head>
 <body>
+
+	<nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <p class="navbar-text">You can easy dwnload code form github</p>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-9">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="https://github.com/myorb/comments" target='_blank'><span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span>  DOWNLOAD </a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <a href="https://github.com/myorb"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
 <?php
 //hook for running code after body tag
 $hooks->run('afterBody');
